20ed9e16aeda341d172fbbaa4fc17e4364119445
Teddy Wing <git@teddywing.com>
2018-08-08 05:30:21 +0200

    Add some types to use for storing the parsed result
    Imagining how the data should be stored after parsing the map file.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+extern crate combine;
+
+use std::collections::HashMap;
+
+type Trigger = String;
+type Action = String;
+
+pub struct Map {
+    pub trigger: Trigger,
+    pub action: Action,
+    pub kind: String,
+}
+
+pub struct DKMapGroup {
+    maps: HashMap<Trigger, Action>,
+    modes: HashMap<Trigger, Map>,
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
