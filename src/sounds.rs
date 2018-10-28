e8ba4dc878a44f14e656664fa2e9d0ea362cbc10
Teddy Wing <git@teddywing.com>
2018-10-28 19:53:50 +0100

    Add `play_audio()` function
    Create a new function that encapsulates the code we experimented with
    for playing audio.
    
    Clean up what we had and handle errors by passing them back in a
    `Result`.
    
    This allows us to encapsulate, audio playing so we can do it in a single
    action during mode switching.

--- src/sounds.rs
+++ src/sounds.rs
@@ -1,4 +1,36 @@
+use std::io::Cursor;
+
+use rodio;
+
+use errors::*;
+
 // const MODE_ACTIVATED = include_bytes!("../sounds/mode_activated.ogg");
 // const MODE_DEACTIVATED = include_bytes!("../sounds/mode_deactivated.ogg");
 
 pub const MODE_ACTIVATED: &'static [u8] = include_bytes!("../sounds/activ.wav");
+
+pub fn play_audio<R>(r: R) -> Result<()>
+where R: AsRef<[u8]> + Send + 'static {
+    let device = rodio::default_output_device()
+        .chain_err(|| "could not find an audio output device")?;
+
+    let reader = Cursor::new(r);
+    let sink = rodio::play_once(&device, reader)
+        .chain_err(|| "error playing audio")?;
+
+    sink.sleep_until_end();
+    sink.play();
+
+    Ok(())
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn play_audio_plays_audio() {
+        play_audio(MODE_ACTIVATED).unwrap();
+    }
+}
